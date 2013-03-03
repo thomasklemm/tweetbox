586e0ab2a1f267b36208c79ae7b5eca846e87fd1
Thomas Klemm <github@tklemm.eu>
2013-03-03 02:08:49 +0100

    Spec project_controller

--- spec/controllers/project_controller_spec.rb
+++ spec/controllers/project_controller_spec.rb
@@ -1,53 +1,101 @@
 require 'spec_helper'
 
-describe ProjectController do
-  controller do
-    def test
-      render text: "42"
-    end
+class ProjectSubController < ProjectController
+  def show
+    render text: '42'
   end
+
+  def login
+    render text: 'login_path'
+  end
+end
+
+describe ProjectSubController do
+  it { should be_a_kind_of(ProjectController) }
 
   def with_test_routing
     with_routing do |map|
       map.draw do
-        get "test" => "anonymous#test"
-        get "login" => "devise/sessions#new", as: :login
+        match "test_route" => "project_sub#show"
+        match "login" => "project_sub#login", as: :login
       end
       yield
     end
   end
 
-  context "unauthenticated user" do
-    before do
-      with_test_routing do
-        get :test
-      end
-    end
+  context "guest user is trying to access" do
+    describe "requires user to sign in" do
 
-    it { should redirect_to(login_path) }
-    it { should set_the_flash }
-    its(:current_user) { should be_blank }
+      it "redirects to login path" do
+        with_test_routing do
+          get :show, id: 1
+          should redirect_to(login_path)
+        end
+      end
+
+      it "sets the flash" do
+        with_test_routing do
+          get :show, id: 1
+          should set_the_flash
+        end
+      end
+
+      it "does not set or have the current user" do
+        with_test_routing do
+          get :show, id: 1
+          expect(controller.current_user).to be_nil
+        end
+      end
+
+    end
   end
 
-  context "project member access" do
-    let(:user)        { Fabricate(:user) }
-    let(:account)     { Fabricate(:account) }
-    let(:membership)  { Fabricate(:membership, user: user, account: account) }
-    let(:project)     { Fabricate(:project, account: account) }
-    let!(:permission) { Fabricate(:permission, membership: membership, project: project) }
+  describe "grants access to project member" do
+    let!(:user)    { Fabricate(:user) }
+    let!(:project) { Fabricate(:project) }
+    let!(:membership) { Fabricate(:membership, user: user, account: project.account) }
+    let!(:permission) { Fabricate(:permission, membership: membership, project: project)  }
 
     before do
       sign_in user
+    end
 
+    it "ensures that user is signed in" do
       with_test_routing do
-        get :test
+        get :show, id: project
+        expect(controller.current_user).to eq(user)
       end
     end
 
-    it { should respond_with(:success) }
-    it { should assign_to(:project) }
-    it { should authorize_resource }
-  end
-end
+    it "loads the project" do
+      with_test_routing do
+        get :show, id: project
+        expect(assigns(:project)).to eq(project)
+      end
+    end
 
-# be_a_kind_of(ProjectController)
+    it "authorizes the resource" do
+      with_test_routing do
+        get :show, id: project
+        should authorize_resource
+      end
+    end
+  end
+
+  describe "forbids access for non-project member" do
+    let!(:user)    { Fabricate(:user) }
+    let!(:project) { Fabricate(:project) }
+    let!(:membership) { Fabricate(:membership, user: user, account: project.account) }
+
+    before do
+      sign_in user
+    end
+
+    it "raises a record not found error" do
+      with_test_routing do
+        expect { get :show, id: project }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
+
+end
