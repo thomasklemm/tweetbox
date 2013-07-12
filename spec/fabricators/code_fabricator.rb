ccac6b2012384facdb7a001231bc8c0b2df22890
Thomas Klemm <github@tklemm.eu>
2013-07-12 10:36:38 +0200

    Remove schema annotations from fabricators, will no longer be updated

--- spec/fabricators/code_fabricator.rb
+++ spec/fabricators/code_fabricator.rb
@@ -1,17 +1,3 @@
-# == Schema Information
-#
-# Table name: codes
-#
-#  created_at :datetime         not null
-#  id         :integer          not null, primary key
-#  tweet_id   :integer
-#  updated_at :datetime         not null
-#
-# Indexes
-#
-#  index_codes_on_tweet_id  (tweet_id)
-#
-
 Fabricator(:code) do
   tweet
 end
--- spec/fabricators/tweet_fabricator.rb
+++ spec/fabricators/tweet_fabricator.rb
@@ -1,30 +1,3 @@
-# == Schema Information
-#
-# Table name: tweets
-#
-#  author_id             :integer          not null
-#  created_at            :datetime         not null
-#  full_text             :text
-#  id                    :integer          not null, primary key
-#  in_reply_to_status_id :integer
-#  in_reply_to_user_id   :integer
-#  previous_tweet_ids    :integer
-#  project_id            :integer          not null
-#  state                 :text
-#  text                  :text
-#  twitter_account_id    :integer          not null
-#  twitter_id            :integer          not null
-#  updated_at            :datetime         not null
-#
-# Indexes
-#
-#  index_tweets_on_previous_tweet_ids         (previous_tweet_ids)
-#  index_tweets_on_project_id                 (project_id)
-#  index_tweets_on_project_id_and_author_id   (project_id,author_id)
-#  index_tweets_on_project_id_and_state       (project_id,state)
-#  index_tweets_on_project_id_and_twitter_id  (project_id,twitter_id) UNIQUE
-#
-
 Fabricator(:tweet) do
   project
   author
