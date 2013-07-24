529fbcf352a7acbc30a597da7f01015be744608a
Thomas Klemm <github@tklemm.eu>
2013-07-24 22:38:27 +0200

    Update config files for application, development and test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,69 +1,43 @@
 Tweetbox::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
+  # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
+  # Do not eager load code on boot.
+  config.eager_load = false
 
-  # Show full error reports and disable caching
+  # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = true
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
 
-  # Print deprecation notices to the Rails logger
+  # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Only use best-standards-support built into browsers
-  config.action_dispatch.best_standards_support = :builtin
+  # Raise an error on page load if there are pending migrations
+  config.active_record.migration_error = :page_load
 
-  # Raise exception on mass assignment protection for Active Record models
-  config.active_record.mass_assignment_sanitizer = :strict
-
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  config.active_record.auto_explain_threshold_in_seconds = 0.5
-
-  # Do not compress assets
-  config.assets.compress = false
-
-  # Expands the lines which load the assets
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
   config.assets.debug = true
 
   # Letter opener
   config.action_mailer.delivery_method = :letter_opener
 
-  # Default actiomailer url host
-  # required by devise
+  # URL defaults
   config.action_mailer.default_url_options = { host: 'tweetbox.dev' }
-
   Rails.application.routes.default_url_options[:host] = 'tweetbox.dev'
 
-  # Quiet Assets
-  config.quiet_assets = true
-
   # Strong Parameters
+  # TODO: See if unnescessary
   config.action_controller.action_on_unpermitted_parameters = :raise
 
   # LiveReload
-  config.middleware.insert_before(
-    ActionDispatch::Static, Rack::LiveReload,
-    host: 'tweetbox.dev'
-  )
-
-  # Bullet
-  # update to the latest bullet config options when enabling
-  # config.after_initialize do
-  #   Bullet.enable = true
-  #   Bullet.alert = true
-  #   Bullet.bullet_logger = true
-  #   Bullet.console = true
-  #   Bullet.rails_logger = true
-  #   Bullet.disable_browser_cache = true
-  # end
+  config.middleware.insert_after(ActionDispatch::Static, Rack::LiveReload)
 end
