933e7e2846de499da6628898afb1846aad4095eb
Thomas Klemm <github@tklemm.eu>
2013-07-26 11:55:12 +0200

    Disable Pusher as deploy is failing

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,6 +1,6 @@
-# Pusher sends live updates to the browser
-require 'pusher'
+# # Pusher sends live updates to the browser
+# require 'pusher'
 
-Pusher.url = ENV['PUSHER_URL']
-Pusher.logger = Rails.logger
-# Pusher.encrypted = true
+# Pusher.url = ENV['PUSHER_URL']
+# Pusher.logger = Rails.logger
+# # Pusher.encrypted = true
