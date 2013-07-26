3cb0ac956cb1abe62a270f7335192873bad48c33
Thomas Klemm <github@tklemm.eu>
2013-07-26 10:45:10 +0200

    Small style changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,6 @@ Tweetbox::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = "http://assets.example.com"
   config.action_controller.asset_host = "https://d1rpufzqj34hzz.cloudfront.net"
 
   # Precompile additional assets.
