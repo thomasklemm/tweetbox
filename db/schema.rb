a07425ad883c98f7719975d1d75baaf463664b61
Philipp Thiel <philipp.thiel@wamf.de>
2013-08-14 13:48:38 +0200

    added favicon pngs with transparancy, started pricing page therefore splitted navigation into pricing and signup

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130809144932) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accounts", force: true do |t|
     t.text     "name",           null: false
     t.datetime "created_at"
