51e085f96dd34bc5f2a00832a57620a118b601f7
Thomas Klemm <github@tklemm.eu>
2013-08-14 23:04:08 +0200

    Remove postgres extension from schema.rb, keeps reappearing

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20130812194225) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "accounts", force: true do |t|
     t.text     "name",           null: false
     t.datetime "created_at"
