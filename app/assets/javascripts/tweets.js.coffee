091209044e8869e108e08c8d11ad68d4c56598b4
Thomas Klemm <github@tklemm.eu>
2013-08-15 17:27:22 +0200

    Incoming tweets update live in two browser windows when one gets resolved

--- app/assets/javascripts/tweets.js.coffee
+++ app/assets/javascripts/tweets.js.coffee
@@ -1,4 +1,13 @@
+# tweets
 @TweetPusher =
-  addTweets: (tweets) ->
-    if tweets.length > 0
-      $('#tweets')
+  updateTweet: (tag, tweet) ->
+    # Replaces multiple tags
+    $(tag).replaceWith(tweet)
+    # Call on entire page to catch all new dom nodes
+    $("abbr.timeago").timeago()
+
+pusher = new Pusher('bd90c5fceef54c5f2c55')
+channel = pusher.subscribe('channel')
+channel.bind 'update-tweet', (data) ->
+  console.log(data)
+  TweetPusher.updateTweet(data.tag, data.tweet)
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -27,16 +27,40 @@ class TweetsController < TweetController
   def resolve
     @tweet.resolve_by(current_user)
 
-    respond_to do |format|
-      # TODO: Add link to tweet to flash message
-      format.html { redirect_to [@project, :tweets], notice: 'Tweet has been resolved.' }
-      format.js do
-        # Reload tweet's event counter cache
-        @tweets = [@tweet.reload]
-        @tweets |= @tweet.previous_tweets if @tweet.previous_tweets.size > 0
-        @tweets |= @tweet.future_tweets if @tweet.future_tweets.size > 0
-      end
-    end
+    # Reload tweet state and events
+    @tweet.reload
+
+    data = {
+      tag: "." + dom_id(@tweet),
+      tweet: render_to_string(partial: 'tweet', locals: {tweet: @tweet})
+    }
+
+    Pusher.trigger('channel', 'update-tweet', data)
+
+    render json: {success: true}
+
+    # respond_to do |format|
+    #   # TODO: Add link to tweet to flash message
+    #   format.html { redirect_to [@project, :tweets], notice: 'Tweet has been resolved.' }
+    #   format.js do
+    #     # Reload tweet's event counter cache
+    #     @tweets = [@tweet.reload]
+    #     @tweets |= @tweet.previous_tweets if @tweet.previous_tweets.size > 0
+    #     @tweets |= @tweet.future_tweets if @tweet.future_tweets.size > 0
+
+    #     string = render_to_string(partial: 'conversation_for_tweet', tweet: @tweets.first)
+    #     raise string.inspect
+
+    #     data = {
+    #       render partial: 'conversation_for_tweet', tweet: @tweet
+    #     }.to_json
+
+    #     # replace all conversation_for_tweet containers in which the tweet is included
+    #     @tweets.each do |tweet|
+    #       Pusher.trigger('channel', 'update-tweet', { dom_id: ('#' + dom_id(tweet, :conversation_for)), tweet: render_to_string(partial: 'conversation_for_tweet', tweet: tweet) })
+    #     end
+    #   end
+    # end
   end
 
   def activate
