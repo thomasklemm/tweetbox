44244eb1258a589db777625c8f423ce67e21c8f2
Thomas Klemm <github@tklemm.eu>
2013-08-30 13:42:59 +0200

    Comment custom maildown renderer

--- config/initializers/maildown.rb
+++ config/initializers/maildown.rb
@@ -1,8 +1,7 @@
 # Maildown::MarkdownEngine.set do |text|
-#   # renderer = Redcarpet::Render::HTML.new(hard_wrap: true)
-#   # carpet = Redcarpet::Markdown.new(renderer, {})
-#   # carpet.render(text).html_safe
+#   renderer = Redcarpet::Render::HTML.new(hard_wrap: true)
+#   carpet = Redcarpet::Markdown.new(renderer, {})
+#   carpet.render(text).html_safe
 
 #   TweetPipeline.new(text).to_html
-#   raise 'Test'
 # end
