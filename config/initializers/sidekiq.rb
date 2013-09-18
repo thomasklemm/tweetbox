591271e000329a30f0bd2e9b02d1aa9f9ba50045
Thomas Klemm <github@tklemm.eu>
2013-09-18 19:41:06 +0200

    Remove unused spec

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,6 @@
-# TODO: REWORK AFTER RAILS4 UPGRADE
 require 'sidekiq/web'
 
 Sidekiq.configure_server do |config|
-
   # ActiveRecord connection pool size
   database_url = ENV['DATABASE_URL']
   if(database_url)
@@ -10,13 +8,8 @@ Sidekiq.configure_server do |config|
     ActiveRecord::Base.establish_connection
   end
 
-  # Poll interval for scheduled jobs in seconds
-  config.poll_interval = 1
+  # Poll interval for scheduled jobs in seconds (default is 15)
+  config.poll_interval = 5
 end
-
-# # HTTP Basic authentication for the Sidekiq Web Interface
-# Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-#   username == 'tweetbox' && password == 'dash'
-# end
 
 # Source: https://github.com/mperham/sidekiq/wiki/Advanced-Options
